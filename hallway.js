0955826d7654377214fc1d04e3c8caa9f5108dff
Jan Horava <czechinthemail@gmail.com>
2013-05-19 22:13:33 -0700

    improved formatting

--- hallway.js
+++ hallway.js
@@ -5,10 +5,15 @@ function resize(){
     width = get('canvas').width = window.innerWidth;
     height = get('canvas').height = window.innerHeight;
 
-    x = width/2;
-    y = height/2;
+    x = width / 2;
+    y = height / 2;
 
-    i = canvas.createLinearGradient(0,height*.05,0,height);
+    i = canvas.createLinearGradient(
+        0,
+        height * .05,
+        0,
+        height
+    );
     i.addColorStop(0,'#000');
     i.addColorStop(1,'#a99');
     canvas.fillStyle = i;
@@ -28,7 +33,7 @@ function resize(){
 var canvas = get('canvas').getContext('2d');
 var height = 0;
 var i = 0;
-var rotate = 90*(Math.PI/180);
+var rotate = 90 * (Math.PI / 180);
 var width = 0;
 var x = 0;
 var y = 0;
