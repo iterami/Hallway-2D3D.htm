79d61be545e5b6b4ed273f5ff8b32bbb59f2a93e
Jan Horava <czechinthemail@gmail.com>
2013-06-19 19:09:41 -0700

    improved formatting

--- js/hallway.js
+++ js/hallway.js
@@ -6,21 +6,21 @@ function resize(){
     width = get('canvas').width = window.innerWidth;
     height = get('canvas').height = window.innerHeight;
 
-    x = width / 2;
-    y = height / 2;
-
     i = canvas.createLinearGradient(
         0,
         height * .05,
         0,
         height
     );
-    i.addColorStop(0,'#000');
-    i.addColorStop(1,'#a99');
+    i.addColorStop(0, '#000');
+    i.addColorStop(1, '#a99');
     canvas.fillStyle = i;
 
     i = 3;
-    canvas.translate(x,y);
+    canvas.translate(
+        width / 2,
+        height / 2
+    );
     do{
         canvas.rotate(rotate);
         canvas.beginPath();
@@ -37,8 +37,8 @@ function resize(){
             width
         );
         canvas.closePath();
-        canvas.fill()
-    }while(i--)
+        canvas.fill();
+    }while(i--);
 }
 
 var canvas = get('canvas').getContext('2d');
@@ -46,9 +46,7 @@ var height = 0;
 var i = 0;
 var rotate = 90 * (Math.PI / 180);
 var width = 0;
-var x = 0;
-var y = 0;
 
 resize();
 
-window.onresize = resize
+window.onresize = resize;
